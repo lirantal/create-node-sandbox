d694fa72b8b00d19a1cbbb7ddc98c86c51d47c82
Liran Tal <liran.tal@gmail.com>
2023-02-07 20:31:35 +0200

    feat: add basic docker container sandbox

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-console.log('dummy')
+import { spawn } from 'node:child_process'
+
+spawn('docker', ['run', '-it', '--entrypoint', 'bash', 'node:18-bullseye-slim'], {
+  stdio: 'inherit'
+})
