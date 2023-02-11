a13d73432c7a07937117a5ae1fa279b20b165be2
Liran Tal <liran.tal@gmail.com>
2023-02-11 20:01:52 +0200

    feat: force no new privileges in container

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,20 @@ import { env } from 'node:process'
 
 const allowedEnvVars = { PATH: env.PATH }
 
-spawn('docker', ['run', '--rm', '-it', '--entrypoint', 'bash', 'node:18-bullseye-slim'], {
-  stdio: 'inherit',
-  env: allowedEnvVars
-})
+spawn(
+  'docker',
+  [
+    'run',
+    '--rm',
+    '-it',
+    '--security-opt',
+    'no-new-privileges',
+    '--entrypoint',
+    'bash',
+    'node:18-bullseye-slim'
+  ],
+  {
+    stdio: 'inherit',
+    env: allowedEnvVars
+  }
+)
