c915238ed484248e95b4dbe6bad0e6f14691b0fb
Liran Tal <liran.tal@gmail.com>
2023-02-11 20:00:35 +0200

    feat: don't expose environment variables into container env

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
 import { spawn } from 'node:child_process'
+import { env } from 'node:process'
+
+const allowedEnvVars = { PATH: env.PATH }
 
 spawn('docker', ['run', '-it', '--entrypoint', 'bash', 'node:18-bullseye-slim'], {
-  stdio: 'inherit'
+  stdio: 'inherit',
+  env: allowedEnvVars
 })
