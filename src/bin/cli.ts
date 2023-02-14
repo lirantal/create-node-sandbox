3149b1bbbcb23407b51d87a438af1631d6c5aad3
Liran Tal <liran.tal@gmail.com>
2023-02-14 19:34:39 +0100

    feat: add new deps and their types declarations (#6)

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
 import { main } from '../main'
+import Gradient from 'gradient-string';
+
+import cfonts from 'cfonts';
+
+console.log()
+console.log(Gradient('cyan', 'pink')('Hello world!'));
 main()
