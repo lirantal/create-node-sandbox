3821af51bee8361916a4bb10783caeee6157b42f
Liran Tal <liran.tal@gmail.com>
2023-02-16 21:11:53 +0200

    test: add coverage for --image cli argument

---
+++ __tests__/dockerSpawnArguments.test.js
@@ -0,0 +1,29 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+
+import { spawn } from 'node:child_process'
+import { main } from '../src/main'
+
+vi.mock('node:child_process')
+
+afterEach(() => {
+  vi.clearAllMocks()
+})
+
+describe('CLI should parse and use command line arguments and map them correctly', () => {
+  it('users can specify their own container image', () => {
+    main({ image: 'node:1.0' })
+
+    expect(spawn).toBeCalledTimes(1)
+    const haveBeenCalledWith = spawn.mock.calls[0]
+    expect(haveBeenCalledWith[1]).toEqual([
+      'run',
+      '--rm',
+      '-it',
+      '--security-opt',
+      'no-new-privileges',
+      '--entrypoint',
+      'bash',
+      'node:1.0'
+    ])
+  })
+})
