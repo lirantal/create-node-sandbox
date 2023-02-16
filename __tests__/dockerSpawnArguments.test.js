3028673747b7d13c4e263df77ae7ded848c00ab3
Liran Tal <liran.tal@gmail.com>
2023-02-16 21:53:59 +0200

    test: add coverage for --node cli

--- __tests__/dockerSpawnArguments.test.js
+++ __tests__/dockerSpawnArguments.test.js
@@ -43,6 +43,23 @@ describe('CLI should parse and use command line arguments and map them correctly
     ])
   })
 
+  it('users who specify a node version will allow to pull that', () => {
+    main({ node: '16' })
+
+    expect(spawn).toBeCalledTimes(1)
+    const haveBeenCalledWith = spawn.mock.calls[0]
+    expect(haveBeenCalledWith[1]).toEqual([
+      'run',
+      '--rm',
+      '-it',
+      '--security-opt',
+      'no-new-privileges',
+      '--entrypoint',
+      'bash',
+      'node:16-bullseye-slim'
+    ])
+  })
+
   it('users who specify both a node image and a node version will learn that image takes precedents', () => {
     main({ node: '16', image: 'node:14-bullseye' })
 
